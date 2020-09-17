8e6a42bd467f0db0b73a63c35c78c19a05d535eb
Pedro Alexandre Pacheco <pedro.pacheco.a@gmail.com>
2020-09-16 22:48:58 -0300

    Acessando BD

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const mongoose = require('mongoose');
+
+let usuario = process.env.NODE_USER
+let senha = process.env.NODE_PASS
+
+if(process.env.NODE_ENV !== 'producao'){
+    require('dotenv').config();
+    usuario = process.env.NODE_USER
+    senha = process.env.NODE_PASS
+}else{
+    usuario = process.env.NODE_USER
+    senha = process.env.NODE_PASS
+};
+
+const strconn = `mongodb+srv://${usuario}:${senha}@cluster0.azubg.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 const app = express();
 
+mongoose.connect(strconn,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>console.log('Conectando com MongoDB'))
+.catch(err => console.log(err));
+
 const indeRoutes = require('./routes/index');
+const { mongo } = require('mongoose');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname,'views'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,48 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
+
+const dadosaneel = new mongoose.Schema({
+    distibuidora: String,
+    codigo:String,
+    titular:String,
+    classe:String,
+    subgrupo:String,
+    modalidade:String,
+    credito:String,
+    municipio:String,
+    uf:String,
+    cep:String,
+    data:String,
+    tipo:String,
+    fonte:String,
+    potencia:String
+});
 
 router.get('/',(req, res)=>{
-    res.render('todasunidades/todasunidades')
+    //res.render('todasunidades/todasunidades')
+    res.redirect('dados/1')
 });
+
+router.get('/dados/:page',(req,res, next)=>{
+    const dtaneel = mongoose.model('dadosaneel',dadosaneel);
+    let perpage = 100;
+    let page = req.params.page || 1;
+
+    dtaneel
+    .find({})
+    .skip((perpage * page)-perpage)
+    .limit(perpage)
+    .exec((err, unidades)=>{
+        dtaneel.countDocuments((err, count)=>{
+            if(err) return next(err);
+            res.render('todasunidades/todasunidades',{
+                products:unidades,
+                current:page,
+                pages:Math.ceil(count/perpage),
+                totalregistros:count
+            })
+        })
+    })
+})
 
 module.exports = router;
